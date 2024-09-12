e014dab06d5d9803cdc866164ecd6b243a6373f5
filipecsoares <filipecarlossoares@gmail.com>
2024-09-12 13:01:00 -0300

    init math-problem service

---
+++ services/math-problem-service/src/test/java/com/mathplatform/math_problem_service/MathProblemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathplatform.math_problem_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathProblemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
