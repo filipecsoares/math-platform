468f198b432a2985d4689bd31cdb5696cd21f25f
filipecsoares <filipecarlossoares@gmail.com>
2024-09-19 08:12:05 -0300

    init solution evaluation service

---
+++ services/solution-evaluation-service/src/main/java/com/mathplatform/solution_evaluation_service/SolutionEvaluationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mathplatform.solution_evaluation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionEvaluationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionEvaluationServiceApplication.class, args);
+	}
+
+}
---
+++ services/solution-evaluation-service/src/test/java/com/mathplatform/solution_evaluation_service/SolutionEvaluationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathplatform.solution_evaluation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionEvaluationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
